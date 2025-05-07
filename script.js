121fad592622659e99a54dca0c359f4f1768fe59
HannahHerz <hannah.staudenherz@outlook.com>
2025-05-07 12:36:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleInfo(id) {
+    const section = document.getElementById(id);
+    section.style.display = section.style.display === "block" ? "none" : "block";
+  }
+  
+  function toggleMore(id) {
+    const more = document.getElementById(id);
+    more.style.display = more.style.display === "block" ? "none" : "block";
+  }
+  
